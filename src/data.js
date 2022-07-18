0794bfdcda66aab1d40d1707d543dba088428fa9
rasool700 <rasoolkarami700@gmail.com>
2022-07-18 20:20:15 +0430

    home page cards added

---
+++ src/data.js
@@ -0,0 +1,89 @@
+import bcrypt from "bcryptjs";
+
+export const products = [
+  {
+    name: "Winter body",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "ارسال رایگان" },
+      { support: "اورجینال" },
+    ],
+    price: 120,
+    offPrice: 120,
+    discount: 0,
+    image: "https://s6.uupload.ir/files/item1_kxtf.jpg",
+  },
+  {
+    name: "Adidas",
+    description: [{ support: "گارانتی مادام العمر" }, { support: "اورجینال" }],
+    price: 110,
+    offPrice: 100,
+    discount: 8,
+    image: "https://s6.uupload.ir/files/item2_azfp.jpg",
+  },
+  {
+    name: "Vans",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 99,
+    offPrice: 89,
+    discount: 10,
+    image: "https://s6.uupload.ir/files/item3_4p29.jpg",
+  },
+  {
+    name: "White",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 260,
+    offPrice: 220,
+    discount: 15,
+    image: "https://s6.uupload.ir/files/item4_uc3.jpg",
+  },
+  {
+    name: "Croopped-shoe",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 150,
+    offPrice: 150,
+    discount: 0,
+    image: "https://s6.uupload.ir/files/item5_r3j1.jpg",
+  },
+  {
+    name: "Blues",
+    description: [
+      { support: "گارانتی مادام العمر" },
+      { support: "اورجینال" },
+      { support: "ارسال رایگان" },
+    ],
+    price: 220,
+    offPrice: 200,
+    discount: 13,
+    image: "https://s6.uupload.ir/files/item6_w6m1.jpg",
+  },
+];
+
+export const users = [
+  {
+    name: "Saheb mohamadi",
+    email: "saheb.ex@gmail.com",
+    password: bcrypt.hashSync("12345", 8),
+    phoneNumber: "09180000000",
+    isAdmin: true,
+  },
+  {
+    name: "John",
+    email: "user@example.com",
+    password: bcrypt.hashSync("1234", 8),
+    isAdmin: false,
+    phoneNumber: "09181230000",
+  },
+];
--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -1,5 +1,25 @@
+import "./HomePage.css";
+import * as data from "../../data";
 const HomePage = () => {
-  return <div>this is home page</div>;
+  return (
+    <section className="products-list-container">
+      {data.products.map((product) => (
+        <div className="product-container">
+          <span className="products-off">${product.discount}</span>
+          <div className="product-card">
+            <img
+              className="product-img"
+              src={product.image}
+              alt="product-img"
+            ></img>
+            <p className="product-name">{product.name}</p>
+            <p className="product-price">${product.price}</p>
+            <button className="products-btn">Add to cart</button>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default HomePage;
